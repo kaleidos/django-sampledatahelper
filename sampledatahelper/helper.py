3a07617fa505fba6c9314f94dc2f739fb14d6c2e
David Barragan <david.barragan@kaleidos.net>
2012-06-28 14:31:00 +0200

    Fixed probles with an empty queryset

--- sampledatahelper/helper.py
+++ sampledatahelper/helper.py
@@ -27,21 +27,21 @@ class SampleDataHelper(object):
         self.ttf_font = ImageFont.truetype(self.app_directory+"/static/font.ttf", 80)
 
         self.images_cache = {}
-    
+
     def word(self):
         """Random text with 1 word."""
         return lorem_ipsum.words(1, common=False)
-        
+
     def words(self, min_words=1, max_words=5):
         """Random text with 1 word."""
         words = random.randint(min_words, max_words)
         return lorem_ipsum.words(words, common=False)
-        
+
     def email(self):
         """Random mail address."""
         return lorem_ipsum.words(1, common=False) + u'@' + lorem_ipsum.words(1, common=False) + \
                random.choice([u'.es', u'.com', u'.org', u'.net', u'.gov', u'.tk'])
-    
+
     def int(self, *args, **kwargs):
         """Random number from 0 or min_value to max_value - 1 or sys.maxint - 1."""
         if len(kwargs.keys()) > 0:
@@ -58,11 +58,11 @@ class SampleDataHelper(object):
             elif len(args) == 1:
                 min_value = 0
                 max_value = args[0]
-            else: 
+            else:
                 min_value = args[0]
                 max_value = args[1]
         return random.randrange(min_value, max_value)
-    
+
     def province_code(self):
         """Random province code."""
         return random.choice(
@@ -73,27 +73,27 @@ class SampleDataHelper(object):
              '41', '42', '43', '44', '45', '46', '47', '48', '49', '50',
              '51', '52', 'AD', ]
         )
-    
+
     def sentence(self):
         """Random text with variable number of words, one sentence."""
         return lorem_ipsum.sentence()[:255]
-    
+
     def paragraph(self):
         """Random text with variable number of words, several sentences."""
-        return lorem_ipsum.paragraph()    
-    
+        return lorem_ipsum.paragraph()
+
     def number(self, ndigits):
         """Random number from 0 to 999[with the given number of digits]."""
         return random.randrange(0, 10 ** ndigits)
-    
+
     def float(self, min, max):
         """Random float from min to max"""
         return (max - (random.random() * (max - min)))
-    
+
     def number_string(self, ndigits):
         """Random number from 0 to ndigits, in string format, filled by 0s on the left."""
         return u''.join(random.choice(u'0123456789') for i in range(ndigits))
-    
+
     def name(self, locale=None, number=1, as_dict=False):
         return Name().generate(self, locale, number, as_dict)
 
@@ -106,13 +106,13 @@ class SampleDataHelper(object):
     def slug(self, min_words=5, max_words=5):
         """Random slug"""
         return "-".join([ self.word() for x in range(self.int(max_value=max_words, min_value=min_words))])
-    
+
     def boolean(self):
         return random.randrange(0, 2) == 0
-    
+
     def choice(self, choices):
         return random.choice(choices)
-    
+
     def image(self, width, height, typ="simple"):
         if typ == "simple":
             generator = ImgSimple()
@@ -136,11 +136,11 @@ class SampleDataHelper(object):
         im_file = ImageFile(open(tfname, 'r'))
 
         return im_file
-    
+
     def future_date(self, min_distance=0, max_distance=365):
         """Random date between today and today + one year - one day."""
         return dt.date.today() + dt.timedelta(random.randrange(min_distance, max_distance))
-    
+
     def past_date(self, min_distance=0, max_distance=365):
         """Random date between today and today + one year - one day."""
         return dt.date.today() - dt.timedelta(random.randrange(min_distance, max_distance))
@@ -148,7 +148,7 @@ class SampleDataHelper(object):
     def future_datetime(self, min_distance=0, max_distance=1440):
         """Random date between today and today + one year - one day."""
         return dt.datetime.utcnow().replace(tzinfo=utc) + dt.timedelta(minutes=random.randrange(min_distance, max_distance))
-    
+
     def past_datetime(self, min_distance=0, max_distance=1440):
         """Random date between today and today + one year - one day."""
         return dt.datetime.utcnow().replace(tzinfo=utc) - dt.timedelta(minutes=random.randrange(min_distance, max_distance))
@@ -169,8 +169,9 @@ class SampleDataHelper(object):
 
     def db_object(self, model):
         return self.db_object_from_queryset(model.objects.all())
-    
+
     def db_object_from_queryset(self, queryset):
         count = queryset.all().count()
-        return queryset.all()[self.int(max_value=count)]
+        return queryset.all()[self.int(max_value=count)] if count > 0 else None
+
 
