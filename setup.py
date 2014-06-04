945c6d05b29d9b22d7ba8e0004b25b5371af47ef
Jesús Espino <jesus.espino@kaleidos.net>
2014-06-04 18:15:03 +0200

    Adding travis nose install line

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
         'versiontools >= 1.8',
     ],
     test_suite = 'nose.collector',
-    tests_require = ['nose >= 1.2.1', 'django >= 1.3.0'],
+    tests_require = ['nose >= 1.3.0', 'django >= 1.3.0'],
     classifiers = [
         "Programming Language :: Python",
         'Development Status :: 4 - Beta',
